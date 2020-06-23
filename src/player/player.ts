5c4c390eaef6b43ba7f45a45bf4e4cf76762471e
Alexei Snytkine <ipodalexei@gmail.com>
2020-06-23 07:15:27 -0400

    initial commit

---
+++ src/player/player.ts
@@ -0,0 +1,11 @@
+export interface Player {
+    stats: {
+        mainStat: number,
+        weaponDamage: number,
+        crit: number,
+        det: number,
+        dhit: number,
+        spellSpeed: number,
+        skillSpeed: number
+    }
+}
