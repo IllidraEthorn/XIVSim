81eed0860035bd25f29be9de26a0db194e05254a
Alexei Snytkine <ipodalexei@gmail.com>
2020-06-23 17:49:23 -0400

    More damage calculation functions

--- src/util/damagecalc.ts
+++ src/util/damagecalc.ts
@@ -1,4 +1,4 @@
-import { LevelMod } from "../consts/levelmod";
+import levelMod80, { LevelMod } from "../consts/levelmod";
 
 //All functions were made using information taken from http://allaganstudies.akhmorning.com/
 const potencyMult = (ptc: number) => {
@@ -15,4 +15,28 @@ const attackPowerMult = (attackPower: number) => {
 
 const detMult = (levelMod: LevelMod, determination: number) => {
     return (Math.floor(130 * (determination - levelMod.main) / levelMod.div + 1000) / 1000)
+}
+
+const tenacityMult = (levelMod: LevelMod, tenacity: number) => {
+    return (Math.floor(100 * (tenacity - levelMod.sub) / levelMod.div + 1000) / 1000)
+}
+
+const speedMult = (levelMod: LevelMod, speed: number) => {
+    return (Math.floor(130 * (speed - levelMod.sub) / levelMod.div + 1000) / 1000)
+}
+
+const directHitChance = (levelMod: LevelMod, dhit: number) => {
+    return (Math.floor(550 * (dhit - levelMod.sub) / levelMod.div) / 10)
+}
+
+const critChance = (levelMod: LevelMod, crit: number) => {
+    return (Math.floor(200 * (crit - levelMod.sub) / levelMod.div + 50) / 10)
+}
+
+const critDamageBonus = (levelMod: LevelMod, crit: number) => {
+    return (Math.floor(200 * (crit - levelMod.sub) / levelMod.div + 1400) / 1000)
+}
+
+const autoAttack = (levelMod: LevelMod, jobMod: number, weaponDamage: number, weaponDelay: number) => {
+    return (Math.floor(Math.floor(levelMod.main * jobMod / 1000 + weaponDamage) * (weaponDelay / 3)))
 }
