6b32070e67bd645b1f472dcf11111ef6d2792eed
Alexei Snytkine <ipodalexei@gmail.com>
2020-06-28 23:34:12 -0400

    Updates

---
+++ precache-manifest.5c74e34fe63e21432f98be4b50e71183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d4ae1ca2167b758e6360b8dbe9146d",
+    "url": "/XIVSim/index.html"
+  },
+  {
+    "revision": "4d26824b1d0aac33c05e",
+    "url": "/XIVSim/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca5e09160bdcef888f8a",
+    "url": "/XIVSim/static/js/2.21b225d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/XIVSim/static/js/2.21b225d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d26824b1d0aac33c05e",
+    "url": "/XIVSim/static/js/main.00da9161.chunk.js"
+  },
+  {
+    "revision": "26beeb349bf58d05db97",
+    "url": "/XIVSim/static/js/runtime-main.44e62228.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/XIVSim/static/media/logo.5d5d9eef.svg"
+  }
+]);
